a1c440bd2c3dc84192e9cd8e6fa9922dd9bdeba4
MuammarRizal <muammarrizal31@gmail.com>
2024-12-02 23:13:15 +0700

    Visibilitas v1.2

--- 5. Visibilitas/index.php
+++ 5. Visibilitas/index.php
@@ -9,10 +9,19 @@
         public function berodah(int $roda=2) : int {
             return $this -> berodah + $roda;
         }
+
+        protected function dataMobil(int $roda, string $jalan) : mixed{
+            return $this -> berodah(). " ".$this -> jalan();
+        }
     }
 
+    function data(): Mobil {
+        return new Mobil();
+    }
+    
     $avanza = new Mobil();
     echo "Menggunakan public : "; echo $avanza -> jalan("4"); echo PHP_EOL;
     echo "Jumlah roda yang digunakan : "; echo $avanza -> berodah(3); echo PHP_EOL;
-    echo $avanza -> roda; // error karena bersifat private
+    echo "Roda mobil : ".$avanza -> roda; echo PHP_EOL; // error karena bersifat private
+    
 ?>
