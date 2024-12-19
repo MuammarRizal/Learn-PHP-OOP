7261874c48c18d4435b0bda63b689ad95486d398
MuammarRizal <muammarrizal31@gmail.com>
2024-12-20 04:32:08 +0700

    composer

---
+++ Project/PPH21/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'modernoop/pph21',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '44fee636b00e7a96e5a7d0025b815e471e8d25b7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'modernoop/pph21' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '44fee636b00e7a96e5a7d0025b815e471e8d25b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
